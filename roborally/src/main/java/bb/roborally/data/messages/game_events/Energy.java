1f9a70dcad2ddad068065cea30f047e2704858ca
Muqiu Wang <wangm@cip.ifi.lmu.de>
2022-06-12 16:13:12 +0200

    Energy class finished

--- roborally/src/main/java/bb/roborally/data/messages/game_events/Energy.java
+++ roborally/src/main/java/bb/roborally/data/messages/game_events/Energy.java
@@ -1,4 +1,47 @@
 package bb.roborally.data.messages.game_events;
 
-public class Energy {
+import bb.roborally.data.messages.Envelope;
+import bb.roborally.data.messages.Message;
+
+public class Energy implements Message {
+    private int clientID;
+    private int count;
+    private String source;
+
+    public Energy() {
+    }
+
+    public int getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(int clientID) {
+        this.clientID = clientID;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    @Override
+    public String toJson() {
+        return toEnvelope().toJson();
+    }
+
+    @Override
+    public Envelope toEnvelope() {
+        return new Envelope(Envelope.MessageType.ENERGY, this);
+    }
 }
