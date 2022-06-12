86a4f3fcc846003bc13b0f5f247c353b01684d5c
Muqiu Wang <wangm@cip.ifi.lmu.de>
2022-06-12 16:26:25 +0200

    CheckPointReachedTypeAdapter finished

--- roborally/src/main/java/bb/roborally/data/messages/type_adapters/game_events/CheckPointReachedTypeAdapter.java
+++ roborally/src/main/java/bb/roborally/data/messages/type_adapters/game_events/CheckPointReachedTypeAdapter.java
@@ -1,4 +1,35 @@
 package bb.roborally.data.messages.type_adapters.game_events;
 
-public class CheckPointReachedTypeAdapter {
+import bb.roborally.data.messages.game_events.CheckPointReached;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+
+public class CheckPointReachedTypeAdapter extends TypeAdapter<CheckPointReached> {
+    @Override
+    public void write(JsonWriter jsonWriter, CheckPointReached checkPointReached) throws IOException {
+        jsonWriter.beginObject();
+        jsonWriter.name("clientID").value(checkPointReached.getClientID());
+        jsonWriter.name("number").value(checkPointReached.getNumber());
+        jsonWriter.endObject();
+    }
+
+    @Override
+    public CheckPointReached read(JsonReader jsonReader) throws IOException {
+        CheckPointReached checkPointReached = new CheckPointReached();
+        jsonReader.beginObject();
+        while(jsonReader.hasNext()){
+            String name = jsonReader.nextName();
+            if(name.equals("clientID")){
+                checkPointReached.setClientID(jsonReader.nextInt());
+            }
+            if(name.equals("number")){
+                checkPointReached.setNumber(jsonReader.nextInt());
+            }
+        }
+        jsonReader.endObject();
+        return checkPointReached;
+    }
 }
