20a2eef9da9f46b08612de323001f956ef47c589
Muqiu Wang <wangm@cip.ifi.lmu.de>
2022-06-12 15:46:29 +0200

    Animation and the corresponding type adapter finished

--- roborally/src/main/java/bb/roborally/data/messages/game_events/Animation.java
+++ roborally/src/main/java/bb/roborally/data/messages/game_events/Animation.java
@@ -1,4 +1,29 @@
 package bb.roborally.data.messages.game_events;
 
-public class Animation {
+import bb.roborally.data.messages.Envelope;
+import bb.roborally.data.messages.Message;
+
+public class Animation implements Message {
+    private String type;
+
+    public Animation(String type) {
+        this.type = type;
+    }
+    public Animation(){}
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toJson() {
+        return toEnvelope().toJson();
+    }
+
+    @Override
+    public Envelope toEnvelope() {
+        return new Envelope(Envelope.MessageType.ANIMATION, this);
+    }
 }
--- roborally/src/main/java/bb/roborally/data/messages/type_adapters/game_events/AnimationTypeAdapter.java
+++ roborally/src/main/java/bb/roborally/data/messages/type_adapters/game_events/AnimationTypeAdapter.java
@@ -1,4 +1,31 @@
 package bb.roborally.data.messages.type_adapters.game_events;
 
-public class AnimationTypeAdapter {
+import bb.roborally.data.messages.game_events.Animation;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+
+public class AnimationTypeAdapter extends TypeAdapter<Animation> {
+    @Override
+    public void write(JsonWriter jsonWriter, Animation animation) throws IOException {
+        jsonWriter.beginObject();
+        jsonWriter.name("type").value(animation.getType());
+        jsonWriter.endObject();
+    }
+
+    @Override
+    public Animation read(JsonReader jsonReader) throws IOException {
+        Animation animation = new Animation();
+        jsonReader.beginObject();
+        while(jsonReader.hasNext()){
+            String name = jsonReader.nextName();
+            if(name.equals("type")){
+                animation.setType(jsonReader.nextString());
+            }
+        }
+        jsonReader.endObject();
+        return animation;
+    }
 }
