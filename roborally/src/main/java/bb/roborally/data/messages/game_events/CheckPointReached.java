5d5e4b5fa25462cf4fcd086ed8471f4da3c83743
Muqiu Wang <wangm@cip.ifi.lmu.de>
2022-06-12 16:22:46 +0200

    CheckPointReached finished

--- roborally/src/main/java/bb/roborally/data/messages/game_events/CheckPointReached.java
+++ roborally/src/main/java/bb/roborally/data/messages/game_events/CheckPointReached.java
@@ -1,4 +1,38 @@
 package bb.roborally.data.messages.game_events;
 
-public class CheckPointReached {
+import bb.roborally.data.messages.Envelope;
+import bb.roborally.data.messages.Message;
+
+public class CheckPointReached implements Message {
+    private int clientID;
+    private int number;
+
+    public CheckPointReached() {
+    }
+
+    public int getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(int clientID) {
+        this.clientID = clientID;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toJson() {
+        return toEnvelope().toJson();
+    }
+
+    @Override
+    public Envelope toEnvelope() {
+        return new Envelope(Envelope.MessageType.CHECK_POINT_REACHED, this);
+    }
 }
