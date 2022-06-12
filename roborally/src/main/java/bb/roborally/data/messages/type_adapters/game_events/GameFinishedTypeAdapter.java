ec10245e8799646ca7f659d32791c9d275138143
Muqiu Wang <wangm@cip.ifi.lmu.de>
2022-06-12 16:37:59 +0200

    GameFinishedTypeAdapter finished

--- roborally/src/main/java/bb/roborally/data/messages/type_adapters/game_events/GameFinishedTypeAdapter.java
+++ roborally/src/main/java/bb/roborally/data/messages/type_adapters/game_events/GameFinishedTypeAdapter.java
@@ -1,4 +1,31 @@
 package bb.roborally.data.messages.type_adapters.game_events;
 
-public class GameFinishedTypeAdapter {
+import bb.roborally.data.messages.game_events.GameFinished;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+
+public class GameFinishedTypeAdapter extends TypeAdapter<GameFinished> {
+    @Override
+    public void write(JsonWriter jsonWriter, GameFinished gameFinished) throws IOException {
+        jsonWriter.beginObject();
+        jsonWriter.name("clientID").value(gameFinished.getClientID());
+        jsonWriter.endObject();
+    }
+
+    @Override
+    public GameFinished read(JsonReader jsonReader) throws IOException {
+        GameFinished gameFinished = new GameFinished();
+        jsonReader.beginObject();
+        while(jsonReader.hasNext()){
+            String name = jsonReader.nextName();
+            if(name.equals("clientID")){
+                gameFinished.setClientID(jsonReader.nextInt());
+            }
+        }
+        jsonReader.endObject();
+        return gameFinished;
+    }
 }
