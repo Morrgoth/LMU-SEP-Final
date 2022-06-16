134b3f71234243b0e45f6fad73db861d4be5954f
Muqiu Wang <wangm@cip.ifi.lmu.de>
2022-06-17 01:52:00 +0200

    EnergySpace, Gear and PushPanel classes finished

--- roborally/src/main/java/bb/roborally/game/tiles/Gear.java
+++ roborally/src/main/java/bb/roborally/game/tiles/Gear.java
@@ -1,5 +1,7 @@
 package bb.roborally.game.tiles;
 
+import bb.roborally.data.messages.game_events.PlayerTurning;
+import bb.roborally.game.Player;
 import bb.roborally.game.Position;
 import bb.roborally.game.Robot;
 
@@ -12,13 +14,40 @@ import bb.roborally.game.Robot;
  * @autor  Philipp Keyzman
  */
 public class Gear extends Tile{
+    final int activationOrder = 4;
     @Override
     String getName() {
         return "Gear";
     }
 
     public enum GearType{
-        GEAR_LEFT,
-        GEAR_RIGHT
+        GEAR_COUNTERCLOCKWISE,
+        GEAR_CLOCKWISE
+    }
+
+    public PlayerTurning turnCounterclockwise(Robot robot){
+        if(robot.getOrientation() == Robot.Orientation.FRONT){
+            robot.setOrientation(Robot.Orientation.LEFT);
+        }else if(robot.getOrientation() == Robot.Orientation.LEFT){
+            robot.setOrientation(Robot.Orientation.BACK);
+        }else if(robot.getOrientation() == Robot.Orientation.BACK){
+            robot.setOrientation(Robot.Orientation.RIGHT);
+        }else if(robot.getOrientation() == Robot.Orientation.RIGHT){
+            robot.setOrientation(Robot.Orientation.FRONT);
+        }
+        return new PlayerTurning(robot.getClientID(), "counterclockwise");
+    }
+
+    public PlayerTurning turnClockwise(Robot robot){
+        if(robot.getOrientation() == Robot.Orientation.FRONT){
+            robot.setOrientation(Robot.Orientation.RIGHT);
+        }else if(robot.getOrientation() == Robot.Orientation.RIGHT){
+            robot.setOrientation(Robot.Orientation.BACK);
+        }else if(robot.getOrientation() == Robot.Orientation.BACK){
+            robot.setOrientation(Robot.Orientation.LEFT);
+        }else if(robot.getOrientation() == Robot.Orientation.LEFT){
+            robot.setOrientation(Robot.Orientation.FRONT);
+        }
+        return new PlayerTurning(robot.getClientID(), "clockwise");
     }
 }
--- roborally/src/main/java/bb/roborally/game/tiles/PushPanel.java
+++ roborally/src/main/java/bb/roborally/game/tiles/PushPanel.java
@@ -1,7 +1,10 @@
 package bb.roborally.game.tiles;
 
+import bb.roborally.data.messages.game_events.Movement;
+import bb.roborally.game.Orientation;
 import bb.roborally.game.Position;
 import bb.roborally.game.Robot;
+
 
 /**
  * @author Veronika Heckel
@@ -13,9 +16,30 @@ import bb.roborally.game.Robot;
  *
  */
 public class PushPanel extends Tile{
+    final int activationOrder = 3;
+    private int labeledNumber1;
+    private int labeledNumber2;
+    private Orientation orientation;
 
     @Override
     String getName() {
         return "PushPanel";
+    }
+
+    public Movement pushPanelEffect(Robot robot, int endRegister){
+        if(endRegister == labeledNumber1 || endRegister == labeledNumber2){
+            if(this.orientation == Orientation.FRONT){
+                return new Movement(robot.getClientID(), 1, 0);
+            }else if(this.orientation == Orientation.RIGHT){
+                return new Movement(robot.getClientID(), 0, 1);
+            }else if(this.orientation == Orientation.BACK){
+                return new Movement(robot.getClientID(), -1, 0);
+            }else if(this.orientation == Orientation.LEFT){
+                return new Movement(robot.getClientID(), 0, -1);
+            }
+        }else{
+            return new Movement(robot.getClientID(), 0, 0);
+        }
+        return null;
     }
 }
