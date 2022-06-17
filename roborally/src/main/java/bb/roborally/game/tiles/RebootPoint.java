6f002ff2392dce0a166118363a1cb01d171ad069
Muqiu Wang <wangm@cip.ifi.lmu.de>
2022-06-17 17:27:03 +0200

    RebootPoint added

---
+++ roborally/src/main/java/bb/roborally/game/tiles/RebootPoint.java
@@ -0,0 +1,19 @@
+package bb.roborally.game.tiles;
+
+import bb.roborally.data.messages.game_events.Reboot;
+import bb.roborally.game.Robot;
+
+import java.util.ArrayList;
+
+public class RebootPoint extends Tile{
+    ArrayList<Robot> rebootQueue = new ArrayList<>();
+    @Override
+    String getName() {
+        return "RebootPoint";
+    }
+
+    public Reboot needToReboot(Robot robot){
+        rebootQueue.add(robot);
+        return new Reboot(robot.getClientID());
+    }
+}
