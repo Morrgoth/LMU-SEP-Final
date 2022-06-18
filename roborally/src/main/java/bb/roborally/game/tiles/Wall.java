45fe2194acc62f9762bd03a57edc095c6ca9e8af
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2022-06-18 22:31:51 +0200

    class Wall; game_event BlockAction; enum Envelope BLOCK_ACTION

--- roborally/src/main/java/bb/roborally/game/tiles/Wall.java
+++ roborally/src/main/java/bb/roborally/game/tiles/Wall.java
@@ -1,7 +1,11 @@
 package bb.roborally.game.tiles;
 
+import bb.roborally.data.messages.type_adapters.game_events.BlockAction;
+import bb.roborally.game.Orientation;
 import bb.roborally.game.Position;
 import bb.roborally.game.Robot;
+
+import java.util.ArrayList;
 
 /**
  * @author Veronika Heckel
@@ -12,14 +16,52 @@ import bb.roborally.game.Robot;
  * @author  Philipp Keyzman
  */
 public class Wall extends Tile{
+    private Position wallPosition;
+    private ArrayList<Orientation> wallOrientation;
+
+    public Wall(){
+
+    }
+    public Wall (Position wallPosition,ArrayList<Orientation> wallOrientation){
+        this.wallPosition = wallPosition;
+        this.wallOrientation = wallOrientation;
+    }
     @Override
     String getName() {
         return "Wall";
     }
 
-    public enum WallType{
-        WALL_STRAIGHT,
-        WALL_ANGULAR
-
+    public Position getWallPosition() {
+        return wallPosition;
     }
+
+    public void setWallPosition(Position wallPosition) {
+        this.wallPosition = wallPosition;
+    }
+
+    public ArrayList<Orientation> getWallOrientation() {
+        return wallOrientation;
+    }
+
+    public void setWallOrientation(ArrayList<Orientation> wallOrientation) {
+        this.wallOrientation = wallOrientation;
+    }
+
+    public BlockAction blockGameAction(Robot robot){
+        if( robot.getPosition() == wallPosition){
+            if(wallOrientation.contains(robot.getRobotOrientation())){
+
+            }
+        }
+        return new BlockAction(BlockAction.clientID);
+    }
+    public BlockAction blockGameAction(Laser boardLaser){
+        if( boardBoardLaser.getBoardLaserPosition() == wallPosition){
+            if(wallOrientation.contains(boardLaser.getBoardLaserOrientation())){
+
+            }
+        }
+        return new BlockAction(BlockAction.clientID);
+    }
+
 }
