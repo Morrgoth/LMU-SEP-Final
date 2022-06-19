3fdb8f0e34074ed84163be5bfdc430f6efd93129
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2022-06-20 00:56:59 +0200

    added StartPoint

--- roborally/src/main/java/bb/roborally/game/tiles/StartPoint.java
+++ roborally/src/main/java/bb/roborally/game/tiles/StartPoint.java
@@ -12,9 +12,38 @@ import bb.roborally.game.Robot;
  * @author  Philipp Keyzman
  */
 public class StartPoint extends Tile{
+    private Position startingPoint;
+    private int robotClientID;
+
+    public StartPoint(){
+
+    }
+
+    public StartPoint(Position startingPoint, Robot robot){
+        this.startingPoint = startingPoint;
+        this.robotClientID = robot.getClientID();
+    }
+
+
 
     @Override
     String getName() {
         return "StartPoint";
+    }
+
+    public int getRobotClientID() {
+        return robotClientID;
+    }
+
+    public void setRobotClientID(int robotClientID) {
+        this.robotClientID = robotClientID;
+    }
+
+    public Position getStartingPoint() {
+        return startingPoint;
+    }
+
+    public void setStartingPoint(Position startingPoint) {
+        this.startingPoint = startingPoint;
     }
 }
