3ddbb613981f7c9861754841dfd724dbdfeee7bb
Veronika <heckelv@cip.ifi.lmu.de>
2022-06-27 15:22:26 +0200

    added BoardTypeAdapter

--- roborally/src/main/java/bb/roborally/data/messages/type_adapters/map/BoardTypeAdapter.java
+++ roborally/src/main/java/bb/roborally/data/messages/type_adapters/map/BoardTypeAdapter.java
@@ -1,4 +1,59 @@
 package bb.roborally.data.messages.type_adapters.map;
 
-public class BoardTypeAdapter {
+import bb.roborally.game.board.Board;
+import bb.roborally.game.tiles.Tile;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class BoardTypeAdapter extends TypeAdapter<Board> {
+    @Override
+    public void write(JsonWriter jsonWriter, Board board) throws IOException {
+        TileTypeAdaptor tileTypeAdaptor = new TileTypeAdaptor();
+        jsonWriter.beginObject();
+        jsonWriter.name("gameMap");
+        jsonWriter.beginArray();
+        for(ArrayList<ArrayList<Tile>> xAndy: board.getGameMap()){
+            jsonWriter.beginArray();
+            for(ArrayList<Tile> cell: xAndy){
+                jsonWriter.beginArray();
+                for(Tile tile: cell){
+                    tileTypeAdaptor.write(jsonWriter, tile);
+                }
+                jsonWriter.endArray();
+            }
+            jsonWriter.endArray();
+        }
+        jsonWriter.endArray();
+        jsonWriter.endObject();
+    }
+
+    @Override
+    public Board read(JsonReader jsonReader) throws IOException {
+        jsonReader.beginObject();
+        ArrayList<ArrayList<ArrayList<Tile>>> map = new ArrayList<>();
+        if(jsonReader.hasNext()){
+            if(jsonReader.nextName().equals("gameMap")){
+                ArrayList<ArrayList<Tile>> xAndy = new ArrayList<>();
+                jsonReader.beginArray();
+                while(jsonReader.hasNext()){
+                    ArrayList<Tile> field = new ArrayList<>();
+                    jsonReader.beginArray();
+                    while(jsonReader.hasNext()){
+                        field.add(new TileTypeAdaptor().read(jsonReader));
+                    }
+                    jsonReader.endArray();
+                    xAndy.add(field);
+                }
+                jsonReader.endArray();
+                map.add(xAndy);
+            }
+        }
+        Board board = new Board(map);
+        jsonReader.endObject();
+        return board;
+    }
 }
