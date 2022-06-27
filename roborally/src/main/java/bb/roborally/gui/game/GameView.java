c9b902684b01409b3f69b78c979c7d7a04a4f2ed
baizeynab <zeynab.baiani@gmail.com>
2022-06-27 17:03:12 +0200

    UI layout

--- roborally/src/main/java/bb/roborally/gui/game/GameView.java
+++ roborally/src/main/java/bb/roborally/gui/game/GameView.java
@@ -1,18 +1,25 @@
 package bb.roborally.gui.game;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 
 public class GameView {
     private GridPane view;
     private ListView<String> chatListView;
     private TextField messageField;
     private Button sendButton;
+    private Label time;
+    private Label phases;
+    private Label gameBoard;
     public GameView() {
         buildUI();
     }
@@ -22,11 +29,45 @@ public class GameView {
         VBox chatContainer = new VBox();
         chatListView = new ListView<>();
         HBox chatFormHolder = new HBox();
+        HBox timer = new HBox();
+        HBox phase = new HBox();
+        HBox gameBoards = new HBox();
+        VBox rightSide = new VBox(timer,phase,chatContainer);
+        VBox leftSide = new VBox(gameBoards);
         messageField = new TextField();
         sendButton = new Button("Send");
+        time = new Label("TIMER");
+        phases = new Label("PHASE");
+        gameBoard = new Label("Game Board");
         chatFormHolder.getChildren().addAll(messageField, sendButton);
+        chatFormHolder.setSpacing(20);
+        timer.setPrefHeight(50);
+        timer.getChildren().addAll(time);
+        phase.setPrefHeight(50);
+        phase.getChildren().addAll(phases);
+        gameBoards.getChildren().addAll(gameBoard);
+        gameBoards.setPrefHeight(500);
+        gameBoards.setPrefWidth(600);
+        timer.setAlignment(Pos.CENTER);
+        phase.setAlignment(Pos.CENTER);
+        gameBoards.setAlignment(Pos.CENTER);
         chatContainer.getChildren().addAll(chatListView, chatFormHolder);
-        view.addRow(0, chatContainer);
+        //rightSide.getChildren().addAll(chatContainer,timer,phase);
+        //leftSide.getChildren().addAll(gameBoards);
+        timer.setStyle("-fx-background-color: red");
+        phase.setStyle("-fx-background-color: green");
+        gameBoards.setStyle("-fx-background-color: blue");
+        rightSide.setAlignment(Pos.BOTTOM_RIGHT);
+        leftSide.setAlignment(Pos.TOP_LEFT);
+        view.addColumn(1,rightSide);
+        //view.addRow(2, chatContainer);
+        //view.addRow(0,timer);
+        //view.addRow(1,phase);
+        //view.setHgap(10);
+        //view.setVgap(10);
+        //view.setPadding(new Insets(10,10,20,20));
+        //view.setAlignment(Pos.BOTTOM_RIGHT);
+        view.addColumn(0,leftSide);
     }
 
     public ListView<String> getChatListView() {
