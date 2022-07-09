8904f951ed67851f686ad2abefa24b28e60ca18d
Veronika <heckelv@cip.ifi.lmu.de>
2022-07-09 18:00:58 +0200

    add Class PowerUpHandler, VirusCardHandler, WormCardHandler
    fill-Methods in VirusDeck and WormDeck

--- roborally/src/main/java/bb/roborally/server/game/deck/VirusDeck.java
+++ roborally/src/main/java/bb/roborally/server/game/deck/VirusDeck.java
@@ -1,4 +1,15 @@
 package bb.roborally.server.game.deck;
 
+import bb.roborally.server.game.cards.PlayingCard;
+import bb.roborally.server.game.cards.Virus;
+
+import java.util.ArrayList;
+
 public class VirusDeck {
+
+    public void fillVirusDeck(ArrayList<PlayingCard> virusDeck){
+        for(int i = 0; i < 18; i++){
+            virusDeck.add(new Virus());
+        }
+    }
 }
--- roborally/src/main/java/bb/roborally/server/game/deck/WormDeck.java
+++ roborally/src/main/java/bb/roborally/server/game/deck/WormDeck.java
@@ -1,4 +1,16 @@
 package bb.roborally.server.game.deck;
 
+import bb.roborally.protocol.game_events.Animation;
+import bb.roborally.server.game.cards.PlayingCard;
+import bb.roborally.server.game.cards.Worm;
+
+import java.util.ArrayList;
+
 public class WormDeck {
+
+    public void fillWormDeck(ArrayList<PlayingCard> wormDeck){
+        for(int i = 0; i < 6; i++){
+            wormDeck.add(new Worm());
+        }
+    }
 }
