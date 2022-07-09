01daa1cb93cfe559963c606b5f562f61e9c16e0e
Veronika <heckelv@cip.ifi.lmu.de>
2022-07-10 00:02:42 +0200

    PoweUpHandler

--- roborally/src/main/java/bb/roborally/server/game/activation/PowerUpHandler.java
+++ roborally/src/main/java/bb/roborally/server/game/activation/PowerUpHandler.java
@@ -1,4 +1,21 @@
 package bb.roborally.server.game.activation;
 
+import bb.roborally.protocol.game_events.Energy;
+import bb.roborally.server.Server;
+import bb.roborally.server.game.*;
+import javafx.geometry.Pos;
+
 public class PowerUpHandler {
+    Server server;
+    Game game;
+    User user;
+    PlayerInventory playerInventory;
+    EnergyCube energyCube;
+    public PowerUpHandler(User user, Game game, Server server, PlayerInventory playerInventory, EnergyCube energyCube){
+        this.user = user;
+        this.game = game;
+        this.server = server;
+        this.playerInventory = playerInventory;
+        this.energyCube = energyCube;
+    }
 }
