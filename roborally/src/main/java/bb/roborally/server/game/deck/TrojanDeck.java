58d4b8fad446114381fba0a9e10c5f4133833cfb
baizeynab <zeynab.baiani@gmail.com>
2022-07-10 19:11:39 +0200

    Deck

--- roborally/src/main/java/bb/roborally/server/game/deck/TrojanDeck.java
+++ roborally/src/main/java/bb/roborally/server/game/deck/TrojanDeck.java
@@ -1,22 +1,32 @@
 package bb.roborally.server.game.deck;
 
 import bb.roborally.server.game.cards.PlayingCard;
+import bb.roborally.server.game.cards.Trojan;
 import bb.roborally.server.game.cards.Virus;
 
 import java.util.ArrayList;
 
 public class TrojanDeck {
+
+    public TrojanDeck(){
+        fillTrojanDeck();
+    }
     public void fillTrojanDeck(ArrayList<PlayingCard> trojanDeck){
         for(int i = 0; i < 11; i++){
-            trojanDeck.add(new Virus());
+            trojanDeck.add(new Trojan());
         }
     }
 
-    public void drawTrojanCard(ArrayList<PlayingCard> trojanDeck){
+
+    public void drawTrojanCard(){
         trojanDeck.remove(0);
     }
 
     public void addTrojanCard(ArrayList<PlayingCard> trojanDeck){
-        trojanDeck.add(new Virus());
+        trojanDeck.add(new Trojan());
+    }
+
+    public ArrayList<PlayingCard> getTrojanDeck(){
+        return trojanDeck;
     }
 }
