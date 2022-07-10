79c4f7d7fa9a0231b56ec9e5e8a41c0eeff22995
engint <Tolga.Engin@campus.lmu.de>
2022-07-10 18:52:01 +0200

    if any robot is in front of the user it should be pushed with the move card as well. this was implementd at Move1Handler

--- roborally/src/main/java/bb/roborally/server/game/activation/Move1Handler.java
+++ roborally/src/main/java/bb/roborally/server/game/activation/Move1Handler.java
@@ -20,19 +20,30 @@ public class Move1Handler {
         this.user = user;
     }
 
-    public void handle() {
+    public static void handle(User user) {
         Robot robot = user.getRobot();
         Position position = robot.getPosition();
-        if (MovementCheck.wallOnSameFieldForwardCheck(user)== false){
-            if (robot.getRobotOrientation() == Orientation.LEFT) {
-                Position nextPosition = new Position(position.getX() - 1, position.getY());
-                robot.setPosition(nextPosition);
-                Movement movement = new Movement(user.getClientID(), nextPosition.getX(), nextPosition.getY());
-                try {
-                    server.broadcast(movement);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+        try{
+            MovementCheck.wallOnSameFieldForwardCheck(user)== true;
+        }
+        catch(Exception wallAhead){
+            System.out.println("road is blocked by wall");
+        }
+        ArrayList<User> users = PlayerQueue.getUsers();
+        for (User otherUser : users) {
+            if(MovementCheck.robotForwardCheck(user, otherUser)){
+                Move1Handler.handle(otherUser);
+            }
+        }
+        if (robot.getRobotOrientation() == Orientation.LEFT) {
+            Position nextPosition = new Position(position.getX() - 1, position.getY());
+            robot.setPosition(nextPosition);
+            Movement movement = new Movement(user.getClientID(), nextPosition.getX(), nextPosition.getY());
+            try {
+                server.broadcast(movement);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             } else if (robot.getRobotOrientation() == Orientation.RIGHT) {
                 Position nextPosition = new Position(position.getX() + 1, position.getY());
                 robot.setPosition(nextPosition);
@@ -63,7 +74,6 @@ public class Move1Handler {
                     throw new RuntimeException(e);
                 }
             }
-        }
 
     }
 }
