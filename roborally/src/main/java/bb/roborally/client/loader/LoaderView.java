f3614b0a15083f26bfe18a6dd18c0cc8bcdb6d8c
bemantec <amentbence@gmail.com>
2022-07-12 15:35:40 +0200

    Adjusted the layout of LoaderView

--- roborally/src/main/java/bb/roborally/client/loader/LoaderView.java
+++ roborally/src/main/java/bb/roborally/client/loader/LoaderView.java
@@ -1,16 +1,36 @@
 package bb.roborally.client.loader;
 
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.util.Duration;
 
 public class LoaderView {
 
-    private Label view = new Label("Loading...");
+    private GridPane view = new GridPane();
 
     public LoaderView() {
-
+        Label label = new Label("Loading...");
+        ImageView imageView = new ImageView(getClass().getResource("/extra/loader-robot-1.png").toExternalForm());
+        imageView.setFitWidth(200);
+        imageView.setFitHeight(241);
+        view.setAlignment(Pos.CENTER);
+        RotateTransition rt = new RotateTransition(Duration.millis(3500), imageView);
+        rt.setByAngle(360);
+        rt.setCycleCount(Animation.INDEFINITE);
+        rt.setInterpolator(Interpolator.LINEAR);
+        rt.play();
+        view.setVgap(30);
+        view.addRow(0, imageView);
+        view.addRow(1, label);
     }
 
-    public Label getView() {
+    public GridPane getView() {
         return view;
     }
 }
