98a8c78dd68cf4111d43fed8d951afca7051f65d
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2022-07-16 15:51:37 +0200

    RebootHandler ver 1.

--- roborally/src/main/java/bb/roborally/server/game/activation/RebootHandler.java
+++ roborally/src/main/java/bb/roborally/server/game/activation/RebootHandler.java
@@ -1,71 +1,98 @@
 package bb.roborally.server.game.activation;
 
+import bb.roborally.protocol.game_events.DrawDamage;
 import bb.roborally.server.Server;
 import bb.roborally.server.game.Game;
 import bb.roborally.server.game.Position;
-import bb.roborally.server.game.Robot;
 import bb.roborally.server.game.User;
 import bb.roborally.server.game.board.Board;
-import bb.roborally.server.game.board.Cell;
-import bb.roborally.server.game.tiles.StartPoint;
+import bb.roborally.server.game.cards.Spam;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class RebootHandler {
 
-    private static RebootHandler rebootHandler = null;
+    //private static RebootHandler rebootHandler = null;
     ArrayList<User> users = new ArrayList<>();
     Server server;
     Game game;
     User user;
-    Board board;
+    //Board board;
 
 
-    private RebootHandler() {}
+    private RebootHandler() {
+    }
 
-    public void handle(){}
+    public void handle() {
+    }
 
-    public void init(Server server, Game game) {}
+    /*
+    public void init(Server server, Game game) {
+    }
+    */
 
     public void addUser(User user) {
         user.setMustReboot(true);
         users.add(user);
     }
 
+    /*
+        check user startingPointgetX()
+        check board.boardName -> set case accordingly
+        compare getX to case -> if equal -> set rebootposition.equals(StartingPoint)
+        add 2 spam Cards
+        else remove to reboot queue.
+     */
+    Spam spam = new Spam();
+
+    /*
+    optional:
+        while(users.size() != 0) -> reboot;
+     */
     public void reboot() {
-        for (User user1: users) {
-            if (user.isMustReboot()) {
+        Position startingPoint = users.get(0).getStartingPoint();
+        int startingX = users.get(0).getStartingPointX();
+        int clientID = users.get(0).getClientID();
+        int boardCase = 0;
 
-                // User gets 2 Spam cards
-                for (int i = 0; i<2; i++ ){
-                    user.getProgrammingDeck().getDiscardPile().add(game.getSpamDeck().getSpamDeck().remove(0));
-                }
+        if (game.getSelectedMap().equals("DeathTrap") && startingX >= 10) {
+            boardCase = 1;
+        }
+        if (game.getSelectedMap().equals("DizzyHighway") && startingX <= 2) {
+            boardCase = 2;
+        }
+        if (game.getSelectedMap().equals("ExtraCrispy") && startingX <= 2) {
+            boardCase = 3;
+        }
+        if (game.getSelectedMap().equals("LostBearings") && startingX <= 2) {
+            boardCase = 4;
+        }
+        if (game.getSelectedMap().equals("Twister") && startingX <= 2) {
+            boardCase = 5;
+        }
 
-                // Position is set to either StartPoint or Reboot point
-                switch (Cell.AMap){
-                    case true:
-                        Position startPoint = new Position(0,0);
-                        user.getRobot().setPosition(startPoint);
-                    case false:
-                        user.getRobot().setPosition(rebootPoint);
 
+        switch (boardCase) {
+            case 1:
+                game.getRobotList().getRobotByFigureId(clientID).setPosition(startingPoint);
+                break;
+            case 2, 3, 4, 5:
+                game.getRobotList().getRobotByFigureId(clientID).setPosition(startingPoint);
+                break;
+        }
 
-                }
-
-                // Choosing reboot direction -> random**********************************
-                    user.getRobot().setRobotOrientation(Random); //random for now
-
-                // Set mustReboot to false
-                user.setMustReboot(false);
+        for (int i = 0; i < 2; i++) {
+            game.getPlayerQueue().getUsers().get(clientID).getProgrammingDeck().addCard(spam, true);
+            try {
+                server.broadcast(new DrawDamage(clientID, "Spam"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
+        user.setMustReboot(false);
+        users.remove(0);
     }
-
-    public static RebootHandler getInstance() {
-        return null;
-    }
-
 }
 
 
