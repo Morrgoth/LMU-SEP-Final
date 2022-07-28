64402300d32da6e7d33c0135421e001fc4cae52b
Veronika <heckelv@cip.ifi.lmu.de>
2022-07-28 17:32:45 +0200

    fix figureID 0-5

--- roborally/src/main/java/bb/roborally/client/robot_selector/RobotRegistry.java
+++ roborally/src/main/java/bb/roborally/client/robot_selector/RobotRegistry.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class RobotRegistry {
 
     ArrayList<Robot> robots = new ArrayList<>();
-    private final Robot twonky = new Robot(1, "Twonky");
-    private final Robot hulk90 = new Robot(2, "Hulk x90");
-    private final Robot hammerBot = new Robot(3, "HammerBot");
-    private final Robot smashBot = new Robot(4, "SmashBot");
-    private final Robot zoomBot = new Robot(5, "ZoomBot");
-    private final Robot spinBot = new Robot(6, "SpinBot");
+    private final Robot twonky = new Robot(0, "Twonky");
+    private final Robot hulk90 = new Robot(1, "Hulk x90");
+    private final Robot hammerBot = new Robot(2, "HammerBot");
+    private final Robot smashBot = new Robot(3, "SmashBot");
+    private final Robot zoomBot = new Robot(4, "ZoomBot");
+    private final Robot spinBot = new Robot(5, "SpinBot");
 
     public RobotRegistry() {
     }
